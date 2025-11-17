fcd3be92c41449fb5855ad5231703003ce1714ca
Yanbing Chen <dearyanbingchen@gmail.com>
2025-11-17 00:28:07 -0600

    update switch view function

--- src/highlight.js
+++
@@ -1,8 +0,0 @@
-// Highlight utilities for path and nodes
-export function highlightPath(linkSel, nodeSel, focusNode) {
-  const A = new Set(focusNode.ancestors());
-  linkSel.classed('highlight', l => A.has(l.source) && A.has(l.target));
-  nodeSel.selectAll('text').classed('highlight', n => A.has(n));
-}
-
-
